3f8cf75e1b5c4378a584764e3979ae248fc22bc2
Serap ekin <88152926+serap944@users.noreply.github.com>
2025-10-28 21:59:04 +0300

    Enhance accordion item toggle functionality
    Refactor accordion functionality to improve clarity and maintainability.

--- script.js
+++ script.js
@@ -54,19 +54,22 @@
   });
 
   
+// Accordion elementlerini seç
 const accordionOgeler = document.querySelectorAll('.accordion-item');
 
 accordionOgeler.forEach((item) => {
     const baslik = item.querySelector('.accordion-baslik');
-
+    
+    // Başlığa tıklandığında çalışacak fonksiyon
     baslik.addEventListener('click', () => {
-        if(item.classList.contains('accordion-acik')) {
+        // Eğer zaten açıksa kapat
+        if(item.classList.contains('accordion-acik')){
             item.classList.remove('accordion-acik');
         } else {
             // Bütün açıkları kapat
             accordionOgeler.forEach(i => i.classList.remove('accordion-acik'));
+            // Tıklananı aç
             item.classList.add('accordion-acik');
         }
     });
 });
-
